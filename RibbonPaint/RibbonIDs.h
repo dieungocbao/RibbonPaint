4fbe62b31c8ab471fd203b794ac1bf21f64058ea
dieungocbao <dieungocbao@gmail.com>
2017-12-15 12:41:47 +0700

    upload code to github

---
+++ RibbonPaint/RibbonIDs.h
@@ -0,0 +1,20 @@
+// *****************************************************************************
+// * This is an automatically generated header file for UI Element definition  *
+// * resource symbols and values. Please do not modify manually.               *
+// *****************************************************************************
+
+#pragma once
+
+#define ID_DRAW_LINE 2 
+#define ID_DRAW_LINE_LabelTitle_RESID 60001
+#define ID_DRAW_RECTANGLE 3 
+#define ID_DRAW_RECTANGLE_LabelTitle_RESID 60002
+#define ID_DRAW_ELLIPSE 4 
+#define ID_DRAW_ELLIPSE_LabelTitle_RESID 60003
+#define cmdTabHome 5 
+#define cmdTabHome_LabelTitle_RESID 60004
+#define cmdGroupShape 6 
+#define cmdGroupShape_LabelTitle_RESID 60005
+#define ID_EXIT 7 
+#define ID_EXIT_LabelTitle_RESID 60006
+#define InternalCmd2_LabelTitle_RESID 60007
